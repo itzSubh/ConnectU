39de1abc618cb6c9d5a41d336cc9d6060f8bbf9a
itzSubh <subhash03id@gmail.com>
2026-06-14 17:11:14 +0530

    Initialized pages and created some routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
+import { Route, Routes } from "react-router-dom"
+
+import Login from "./pages/Login"
+import Messages from "./pages/Messages"
+import ChatBox from "./pages/ChatBox"
+import Connections from "./pages/Connections"
+import Discover from "./pages/Discover"
+import Profile from "./pages/Profile"
+import CreatePost from "./pages/CreatePost"
+import Feed from "./pages/Feed"
 
 
 function App() {
 
+
+
   return (
-    <div className="text-amber-300">Hello World</div>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
