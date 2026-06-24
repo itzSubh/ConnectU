c8600c7d9a2a2b91fcd94913e77ebe1dee0fe46a
itzSubh <subhash03id@gmail.com>
2026-06-24 08:29:09 +0530

    email reminder added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/user.model.js";
+import Connection from "../models/connection.model.js";
+import sendEmail from "../config/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "connectu" });
@@ -74,9 +76,55 @@ const syncUserDeletion = inngest.createFunction(
         await User.findByIdAndDelete(id)
     }
 )
+
+// Inngest function to send reminder when a new connection request is added
+
+const sendNewConnectionRequestReminder = inngest.createFunction(
+  { id: "send-new-connection-request-reminder", triggers: [{event: "app/connection-request"}]},
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+
+    await step.run('send-connection-request-mail', async() => {
+        const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+        const subject = `👋 New Connection Request`;
+        const body = `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+            <h2>Hi ${connection.to_user_id.full_name},</h2>
+
+              <p>
+                You have a new connection request from
+                ${connection.from_user_id.full_name}
+                (@${connection.from_user_id.username})
+              </p>
+
+              <p>
+                Click <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981;">here</a>
+                to accept or reject the request
+              </p>
+              <br/>
+              <p>
+                Thanks,<br/>
+                ConnnectU - Stay Connected
+              </p>
+        </div>
+        `;
+        await sendEmail({
+            to: connection.to_user_id.email,
+            subject,
+            body
+        })
+
+        return {message: 'Reminder Sent'}
+    })
+
+
+       
+    }
+);
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    sendNewConnectionRequestReminder
 ];
